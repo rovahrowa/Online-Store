c78aaf9e982b349b4fecaa4be401e35926f05c8a
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-06-13 01:35:01 +0300

    Adds the stores router

---
+++ routes/storeController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by danstan on 6/13/17.
+ */
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req,res) =>{
+})
+
+module.exports = router;
