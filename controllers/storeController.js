afcf7252a3c0a7e92b98ac5b99fcd3de9074625e
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-06-14 10:28:53 +0300

    Adds stores endpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -17,8 +17,13 @@ exports.addStore = (req,res) =>{
 }
 
 exports.createStore = async (req,res) =>{
-    const store = new Store(req.body)
-    await store.save()
+    const store = await (new Store(req.body).save())
     req.flash('success', 'Successfully Created New Store')
-    res.redirect('/')
+    res.redirect('/store/${store.slug}')
+}
+
+exports.getStores = async (req,res) =>{
+    const stores = await( Store.find())
+    console.log(stores)
+    res.render('stores', {title:"Stores", stores:stores})
 }
